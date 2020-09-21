a84171b81cdc52c35e6bc765a5b1a11a48afd471
Ирина Бобина <bobin.ir495@gmail.com>
2020-09-21 11:37:09 +0300

    gfbfcv

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import CreateView #импортируем CreateView, чтобы создать ему наследника
-from django.urls import reverse_lazy #эта функция позволит получить URL по имени функции
-from .forms import CreationForm #импортируем класс формы, чтобы сослаться на него во view-классе
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from users.forms import CreationForm
+
 
 class SignUp(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy("login") #  где login — это параметр "name" в path()
+    success_url = reverse_lazy("login")
     template_name = "signup.html"
