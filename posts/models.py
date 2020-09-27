d85a1a406aadd600b25f17b402a942eb708c10b2
Ирина Бобина <bobin.ir495@gmail.com>
2020-09-28 00:36:10 +0300

    uu

--- posts/models.py
+++ posts/models.py
@@ -10,21 +10,21 @@ class Group(models.Model):
     description = models.TextField()
 
     def __str__(self):
-	    return self.title
+        return self.title
 
 
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(
-	    User, on_delete=models.CASCADE, related_name="posts")
-	group = models.ForeignKey(
-	    Group, on_delete=models.SET_NULL, related_name="posts", blank=True,
-	    null=True
-	)
+        User, on_delete=models.CASCADE, related_name="posts")
+    group = models.ForeignKey(
+        Group, on_delete=models.SET_NULL, related_name="posts", blank=True,
+        null=True
+    )
 
     def __str__(self):
-	    return self.text
+        return self.text
 
     class Meta:
         ordering = ("-pub_date",)
