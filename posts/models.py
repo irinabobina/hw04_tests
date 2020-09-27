24af1d6a5ea929804acd94d7834904efa72101de
Ирина Бобина <bobin.ir495@gmail.com>
2020-09-28 00:16:41 +0300

    commit!!!!!!!

--- posts/models.py
+++ posts/models.py
@@ -5,26 +5,26 @@ User = get_user_model()
 
 
 class Group(models.Model):
-	title = models.CharField(max_length=200)
-	slug = models.SlugField(unique=True)
-	description = models.TextField()
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+	    return self.title
 
 
 class Post(models.Model):
-	text = models.TextField()
-	pub_date = models.DateTimeField("date published", auto_now_add=True)
-	author = models.ForeignKey(
-		User, on_delete=models.CASCADE, related_name="posts")
+    text = models.TextField()
+    pub_date = models.DateTimeField("date published", auto_now_add=True)
+    author = models.ForeignKey(
+	    User, on_delete=models.CASCADE, related_name="posts")
 	group = models.ForeignKey(
-		Group, on_delete=models.SET_NULL, related_name="posts", blank=True,
-		null=True
+	    Group, on_delete=models.SET_NULL, related_name="posts", blank=True,
+	    null=True
 	)
 
-	def __str__(self):
-		return self.text
+    def __str__(self):
+	    return self.text
 
     class Meta:
         ordering = ("-pub_date",)
