56b5c7f8f559ac5434bb265d96d6892cfa19dcd9
Ирина Бобина <bobin.ir495@gmail.com>
2020-11-08 00:15:16 +0300

    hbkjhnk

--- posts/views.py
+++ posts/views.py
@@ -6,19 +6,21 @@ from django.utils import timezone
 from .forms import PostForm
 from .models import Group, Post, User
 
+#class Meta:
+#ordering = Post.objects.order_by("-pub_date")
 
-
-
-def get_paginated_view(request, post_list, page_size=10):
-    paginator = Paginator(post_list, page_size)
-    page_number = request.GET.get("page")
-    page = paginator.get_page(page_number)
-    return page, paginator
+#def get_paginated_view(request, post_list, page_size=10):
+#paginator = Paginator(post_list, page_size)
+#page_number = request.GET.get("page")
+#page = paginator.get_page(page_number)
+#return page, paginator
 
 
 def index(request):
-    post_list = Meta.ordering.all()
-    page, paginator = get_paginated_view(request, post_list)
+    post_list = Post.objects.order_by("-pub_date")
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get("page")
+    page = paginator.get_page(page_number)
     context = {"page": page, "paginator": paginator}
     return render(request, "index.html", context)
 
@@ -26,13 +28,17 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     post_list = group.posts.order_by("-pub_date").all()
-    page, paginator = get_paginated_view(request, post_list)
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get("page")
+    page = paginator.get_page(page_number)
     context = {"group": group, "page": page, "paginator": paginator}
     return render(request, "group.html", context)
 
 
 @login_required
 def new_post(request):
+
+#if request.method == "POST":
     form = PostForm(request.POST or None)
     if form.is_valid():
         post = form.save(commit=False)
@@ -40,14 +46,18 @@ def new_post(request):
         post.pub_date = timezone.now()
         post.save()
         return redirect("index")
-
     return render(request, "new_post.html", {"form": form})
+
+#form = PostForm()
+#return render(request, "new_post.html", {"form": form})
 
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     post_list = author.posts.order_by("-pub_date").all()
-    page, paginator = get_paginated_view(request, post_list)
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get("page")
+    page = paginator.get_page(page_number)
     return render(request, "profile.html",
                   {"page": page, "paginator": paginator, "author": author})
 
@@ -60,13 +70,19 @@ def post_view(request, username, post_id):
 @login_required
 def post_edit(request, username, post_id):
 
+#user = get_object_or_404(User, username=username)
+#if request.user != user:
+#return redirect("post_detail", username=username, post_id=post_id)
+
+
     post = get_object_or_404(Post, pk=post_id, author__username=username)
     form = PostForm(request.POST or None, instance=post)
 
-    
+
+#if request.method == "POST":
     if form.is_valid():
         post.pub_date = timezone.now()
         form.save()
         return redirect("post_detail", username=username, post_id=post.pk)
     return render(request, "new_post.html", {"form": form, "post": post})
-  
+#return render(request, "new_post.html", {"form": form, "post": post})
