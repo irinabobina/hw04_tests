6fa748ed317e37094e23a0ed1565457e5f869515
Ирина Бобина <bobin.ir495@gmail.com>
2020-11-09 18:30:18 +0300

    jnk

--- posts/urls.py
+++ posts/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('<username>/', views.profile, name='profile'),
     path('<username>/<int:post_id>/', views.post_view, name='post_detail'),
     path('<username>/<int:post_id>/edit/', views.post_edit, name='post_edit'),
-    path('<username>/group/<int:post_id>/edit/', views.post_edit, name='post_in_gr_edit'),
 ]
